1f814b0021f6b64377bcccc2384809db8e39d782
Malc94 <104456212+malc94@users.noreply.github.com>
2023-07-12 10:37:15 -0300

    Atualizar o tooltips.js

--- js/tooltips.js
+++ js/tooltips.js
@@ -52,7 +52,7 @@ const tooltips = {
 
     // Fundamentals
     "Concentração": "Improve your learning speed through practising intense concentration activities.",
-    "Productivity": "Learn to procrastinate less at work and receive more job experience per day.",
+    "Produtividade": "Learn to procrastinate less at work and receive more job experience per day.",
     "Bargaining": "Study the tricks of the trade and persuasive skills to lower any type of expense.",
     "Meditation": "Fill your mind with peace and tranquility to tap into greater happiness from within.",
 
