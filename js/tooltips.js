7e6c99f572374e90a77b32eec445a9078b502e5f
Malc94 <104456212+malc94@users.noreply.github.com>
2023-07-12 15:00:20 -0300

    Update tooltips.js

--- js/tooltips.js
+++ js/tooltips.js
@@ -113,7 +113,7 @@ const tooltips = {
 
     // Properties
     "Sem-teto": "Sleep on the uncomfortable, filthy streets while almost freezing to death every night. It cannot get any worse than this.",
-    "Tent": "A thin sheet of tattered cloth held up by a couple of feeble, wooden sticks. Horrible living conditions but at least you have a roof over your head.",
+    "Barraca": "A thin sheet of tattered cloth held up by a couple of feeble, wooden sticks. Horrible living conditions but at least you have a roof over your head.",
     "Wooden Hut": "Shabby logs and dirty hay glued together with horse manure. Much more sturdy than a tent, however, the stench isn't very pleasant.",
     "Cottage": "Structured with a timber frame and a thatched roof. Provides decent living conditions for a fair price.",
     "House": "A building formed from stone bricks and sturdy timber, which contains a few rooms. Although quite expensive, it is a comfortable abode.",
