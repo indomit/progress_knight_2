d1bf9f0faa00b1e8a9eb0899a4e625dc5e1a64d0
indomit <indomit@gmail.com>
2024-08-05 02:47:21 +0700

    4xmpp gain in 1e200 milestone description

--- js/milestones.js
+++ js/milestones.js
@@ -45,7 +45,7 @@ const milestoneBaseData = {
     "Ruler of the Metaverse": { name: "Ruler of the Metaverse", expense: 1e90, tier: 38, description: "Unlocks Metaverse Perks, Metaverse Guards Job Category" },
     "A New Hope": { name: "A New Hope", expense: 1e95, tier: 39, description: "Faint Hope always at maximum" },
     "Time is a flat circle": { name: "Time is a flat circle", expense: 1e100, tier: 40, description: "Multiply Time Warping by 1000x, Multiply all xp gain by 1e50x" },
-    "The End is near": { name: "The End is near", expense: 1e200, tier: 50, description: "Unspent Multiverse Perk Points buffs Dark Matter" },
+    "The End is near": { name: "The End is near", expense: 1e200, tier: 50, description: "Unspent Multiverse Perk Points buffs Dark Matter, 4x MPP gain" },
     "The End": { name: "The End", expense: 1e300, tier: 99, description: "Congratulations! You have beaten the game!" },
 }
 
